a8e32cd54e8937bd63d45add8e199de9ddd5647c
BilaStorm <clementbarret79@gmail.com>
2026-01-06 09:54:01 +0100

    Ajout_Fichier_Test_V.0.0.4

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,                 // te permet d'utiliser describe/it sans importer
+    environment: 'jsdom',          // nécessaire pour tester des composants React
+    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'], // inclut tous les fichiers de test
+  },
 })
