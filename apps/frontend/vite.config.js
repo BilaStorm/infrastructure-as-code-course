6521a31e3cca50d41a33a7df4edbd3bbe37ead6b
BilaStorm <clementbarret79@gmail.com>
2026-01-06 15:23:42 +0100

    Ajout_HOST_TRUE_V0.0.10

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+  },
   test: {
     globals: true,                 // te permet d'utiliser describe/it sans importer
     environment: 'jsdom',          // nécessaire pour tester des composants React
